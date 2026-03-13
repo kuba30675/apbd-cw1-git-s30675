eff558339215e18fc016ead35f6a52d58f57498a
Kuba <s30675@pjwstk.edu.pl>
2026-03-13 21:25:48 +0100

    ADD HEIGHT FIELD TO TRIANGLE CLASS
    - Added double height field to the triangle class and a getter for it

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,14 +1,20 @@
 //represents equilateral triangle
 public class Triangle extends Shape{
     private double a;
+    private double height;
 
     public Triangle(double a) {
         super(SHAPETYPE.TRIANGLE);
         this.a = a;
+        this.height = (a * Math.sqrt(3)) / 2;
     }
 
     public double getA() {
         return a;
+    }
+
+    public double getHeight() {
+        return height;
     }
 
     @Override
