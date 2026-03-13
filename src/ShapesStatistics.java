ebf21901cf7088aea0adfe0287c033763ad14f08
Kuba <s30675@pjwstk.edu.pl>
2026-03-13 19:25:04 +0100

    ADD COUNTSHAPES METHOD TO SHAPESSTATISTICS CLASS
    - Added a method Map<Shape,Long> countShapesCreated(List<Shape>) that
      takes a list of shapes and returns map that holds number of created
      shapes for each shape type

--- src/ShapesStatistics.java
+++ src/ShapesStatistics.java
@@ -1,2 +1,16 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public final class ShapesStatistics {
+
+    public static Map<Shape,Long> countShapesCreated(List<Shape> list){
+        return list.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                s -> s,
+                                Collectors.counting()
+                        )
+                );
+    }
 }
